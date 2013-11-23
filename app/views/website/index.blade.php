e044f2a2222e8ffb074b47177e3f0417e662a375
Nico Penaredondo <npenaredondo@gmail.com>
2013-11-23 12:06:31 +0800

    added a full background image header

--- app/views/website/index.blade.php
+++ app/views/website/index.blade.php
@@ -1,4 +1,16 @@
-@extends('layout.master')
+@extends('website.layout.master')
 @section('title')
 	Highly Succeed Ltd | DARTS v0.1
+@stop
+@section('styles')
+	{{ HTML::style('assets/stylesheets/website.css') }}
+@stop
+@section('header')
+	<div id="top" class="header">
+      <div class="vert-text">
+      	<img src="{{ asset('assets/img/company_logo.png') }}" alt="Highly Succeed Ltd.">
+        <h1 style="color:#333333">Built in Bootstrap</h1>
+        <a href="{{ URL::to('auth/login') }}" class="btn btn-default btn-lg">Start Today</a>
+      </div>
+    </div>
 @stop
