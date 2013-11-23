7b0e4cce2b243f0ad111b0c77ea35011c7534e8a
Nico Penaredondo <npenaredondo@gmail.com>
2013-11-23 22:01:33 +0800

    added some info in dashboard

---
+++ app/views/dashboard/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.dar')
+@section('title')
+	Highly Succeed Ltd | DARTS v0.1
+@stop
+
+@section('content')
+	<div class="row">
+	    <div class="col-lg-12">
+	    	<h1>Dashboard <small>Statistics Overview</small></h1>
+	    	<ol class="breadcrumb">
+	            <li class="active"><i class="fa fa-dashboard"></i> Dashboard</li>
+	        </ol>
+	    <div class="alert alert-success alert-dismissable">
+	        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+	        <b>Lorem ipsum dolor sit amet</b>, consectetur adipisicing elit. Quis, eum, consequatur commodi minima quae assumenda ut aut. Dignissimos, sequi veniam rem iure temporibus velit corporis nisi sunt perspiciatis delectus. Debitis.   
+	        </div>
+	    </div>
+	</div><!-- /.row -->
+@stop
